f43c1a065cb790c6509206710b3e1de8dbaa4e47
Daviddemarville <de.marville.david@gmail.com>
2025-07-21 14:21:55 +0200

    add fix to deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/idealy/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
